b04ed83fdabd992be443b6d7fec4fedca5e6ec52
Krishan Patel <sc17kdp@leeds.ac.uk>
2020-03-11 11:33:02 +0000

    changed teacher to professor

--- cwk1/cwk1/RateMyProf/admin.py
+++ cwk1/cwk1/RateMyProf/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import models as md
-from .models import Teacher, Module, Rating
+from .models import Professor, ModuleInstance, Rating
 
-admin.site.register(Teacher)
-admin.site.register(Module)
+admin.site.register(Professor)
+admin.site.register(ModuleInstance)
 admin.site.register(Rating)
--- cwk1/cwk1/RateMyProf/models.py
+++ cwk1/cwk1/RateMyProf/models.py
@@ -6,21 +6,21 @@ import datetime
 
 # Create your models here.
 
-class Teacher(models.Model):
-    t_ID = models.CharField('Teacher ID', max_length=3, unique= True)
-    t_name = models.CharField('Teacher First Name', max_length=120)
-    t_last_Name = models.CharField('Teacher Last Name', max_length=120)
+class Professor(models.Model):
+    t_ID = models.CharField('Professor ID', max_length=3, unique= True)
+    t_name = models.CharField('Professor First Name', max_length=120)
+    t_last_Name = models.CharField('Professor Last Name', max_length=120)
 
     def __str__(self):
         return u'%s %s' % (self.t_name, self.t_last_Name)
 
-class Module(models.Model):
+class ModuleInstance(models.Model):
     Semesters = [('1',1),('2',2)]
     module_ID = models.CharField('Module ID', max_length=3, default = "")
     name = models.CharField('Module Name', max_length=120, default = "")
     semester = models.CharField('Semester', max_length=1, choices = Semesters, default = 1)
     year = models.IntegerField(default = datetime.date.today().year, validators = [MinValueValidator(1904), MaxValueValidator(datetime.date.today().year)])
-    teachers = models.ManyToManyField(Teacher)
+    profs = models.ManyToManyField(Professor)
 
     class Meta:
         unique_together = ('module_ID','name', 'semester', 'year')
@@ -29,9 +29,9 @@ class Module(models.Model):
         return u'%s %s' % (self.name,self.year)
 
 class Rating(models.Model):
-    module = models.ForeignKey(Module,on_delete=models.PROTECT, default = "")
-    teacher = models.ForeignKey(Teacher,on_delete=models.PROTECT, default = "")
+    module = models.ForeignKey(ModuleInstance,on_delete=models.PROTECT, default = "")
+    prof = models.ForeignKey(Professor,on_delete=models.PROTECT, default = "")
     Rating = models.FloatField(default = 1)
 
     def __str__(self):
-        return u'%s,    %s,  Score : %d' % (self.module,self.teacher, self.Rating)
+        return u'%s,    %s,  Score : %d' % (self.module,self.prof, self.Rating)
